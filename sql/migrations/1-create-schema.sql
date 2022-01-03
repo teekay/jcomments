ad373de6a186c6083ae670e07a17bd6354118234
Tomas Kohl <tomas.kohl@gmail.com>
2022-01-03 17:17:28 +0100

    Fix migrations

--- sql/migrations/1-create-schema.sql
+++ sql/migrations/1-create-schema.sql
@@ -4,8 +4,6 @@ CREATE TABLE accounts(id CHARACTER(36) NOT NULL PRIMARY KEY, username VARCHAR(10
 
 CREATE TABLE account_settings(id CHARACTER(36) NOT NULL PRIMARY KEY, account_id CHARACTER(36) NOT NULL REFERENCES accounts(id), blog_url VARCHAR(1024), akismet_key VARCHAR(256), use_akismet BOOLEAN DEFAULT false);
 
-CREATE TABLE account_email_settings(id CHARACTER(36) NOT NULL PRIMARY KEY, account_id CHARACTER(36) NOT NULL REFERENCES accounts(id), notify_on_comments BOOLEAN DEFAULT false, send_comments_digest BOOLEAN DEFAULT false);
-
 CREATE TABLE tokens(id CHARACTER(36) NOT NULL PRIMARY KEY, account_id CHARACTER(36) NOT NULL REFERENCES accounts(id), token VARCHAR(512) NOT NULL, created_at TIMESTAMP NOT NULL, revoked_at TIMESTAMP DEFAULT NULL);
 
 CREATE TABLE comments(id CHARACTER(36) NOT NULL PRIMARY KEY, account_id CHARACTER(36) NOT NULL REFERENCES accounts(id), page_url VARCHAR(2048) NOT NULL, comment TEXT NOT NULL, reader_name VARCHAR(512) NOT NULL, reader_email VARCHAR(512) DEFAULT NULL, reader_website VARCHAR(1024) DEFAULT NULL, created_at TIMESTAMP NOT NULL);
