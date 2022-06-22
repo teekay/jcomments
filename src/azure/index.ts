2636d4fa961f7cc9ecf0180a8f2d3c1336ed7849
Tomas Kohl <tomas.kohl@gmail.com>
2022-06-22 12:14:32 +0200

    Remove DB migration from funcapp init, enable CORS in Azure

--- src/azure/index.ts
+++ src/azure/index.ts
@@ -1,6 +1,5 @@
 import { ApiModule } from '../api/api.module'
 import { INestApplicationContext, Logger } from '@nestjs/common'
-import { migrate } from 'postgres-migrations'
 import { NestFactory } from '@nestjs/core'
 
 export async function bootstrap(): Promise<INestApplicationContext> {
@@ -8,9 +7,6 @@ export async function bootstrap(): Promise<INestApplicationContext> {
   const logger = app.get(Logger)
   app.useLogger(logger)
   app.enableShutdownHooks()
-  const client = app.get('PG_CLIENT')
-  // TODO move migration to a post-deployment task
-  await migrate({ client }, `${__dirname}/../../../sql/migrations`)
 
   return app
 }
