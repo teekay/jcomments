46b4e8fb62739b28f6ea9ddba2367628fb90bfe1
teekay <tomas.kohl@gmail.com>
2026-02-01 09:27:25 -0500

    Fix the function app

--- src/azure/accounts.module.ts
+++ src/azure/accounts.module.ts
@@ -1,13 +1,14 @@
 import { AccountService } from '../shared/accounts/account.service'
 import { AkismetService } from '../shared/comments/akismet.service'
 import { AzureCommentsModule } from './comments.module'
+import { CryptoModule } from '../shared/crypto/crypto.module'
 import { forwardRef, Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
 import { PersistenceModule } from '../shared/persistence/persistence.module'
 import { TokenService } from '../shared/accounts/token.service'
 
 @Module({
-  imports: [forwardRef(() => AzureCommentsModule), PersistenceModule, PassportModule],
+  imports: [forwardRef(() => AzureCommentsModule), CryptoModule, PersistenceModule, PassportModule],
   controllers: [],
   providers: [AccountService, TokenService, AkismetService],
   exports: [AccountService, TokenService, AkismetService],
--- src/azure/auth.module.ts
+++ src/azure/auth.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common'
 import { AuthService } from '../shared/auth/auth.service';
 import { PersistenceModule } from '../shared/persistence/persistence.module'
 import { ConfigModule } from '../shared/config/config.module';
+import { CryptoModule } from '../shared/crypto/crypto.module';
 import { EmailsModule } from '../shared/emails/emails.module';
 import { AzureAccountsModule } from './accounts.module';
 
 @Module({
-   imports: [AzureAccountsModule, ConfigModule, EmailsModule, PersistenceModule],
+   imports: [AzureAccountsModule, ConfigModule, CryptoModule, EmailsModule, PersistenceModule],
    controllers: [],
    providers: [AuthService],
    exports: [AuthService],
